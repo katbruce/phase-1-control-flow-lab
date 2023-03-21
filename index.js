10cbbe46ecf499fc569fb73fe01d020f9ffebc0b
Kathryn <kathrynbruce7@gmail.com>
2023-03-21 09:45:40 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance<400){
+    return('This one is on me!')
+  }
+  else if(distance <= 2000){
+    return('That will be twenty bucks.')
+  }else if (distance>2500){
+    return('No can do.')
+  }else if(distance>2000){
+    return('I will gladly take your thirty bucks.')
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(place){
   // Write your code here!
+  if(place=="NYC"){
+    return('Ok, sounds good.')
+  }else{
+    return('No go.')
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return('Thank you so much.');
+      break;
+    case 'not as generous':
+      return('Thank you.');
+      break;
+    default:
+      return('Bye.')
+  }
 }
